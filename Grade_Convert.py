35147de3cd739d3e9c5c1abd940c6b8dd8b98d35
Daniel Ferdiansyah <127747878+danferdiansyah@users.noreply.github.com>
2023-07-21 22:48:41 +0700

    Add files via upload

---
+++ Grade_Convert.py
@@ -0,0 +1,38 @@
+def scoreconv():
+    score=(input("Masukkan nilai anda "))
+    scorefin=int(score)
+    if scorefin>100:
+        print("Nilai yang anda masukkan invalid")
+    elif scorefin>85:
+        print("Grade anda adalah A")
+    elif scorefin>80:
+        print("Grade anda adalah A-")
+    elif scorefin>75:
+        print("Grade anda adalah B")
+    elif scorefin>70:
+        print("Grade anda adalah B-")
+    elif scorefin<70:
+        print("Anda harus mengulang")
+    elif scorefin<0:
+        print("Nilai yang anda masukkan invalid")
+while True:
+    score=(input("Masukkan nilai anda "))
+    scorefin=int(score)
+    if scorefin>100:
+        print("Nilai yang anda masukkan invalid")
+    elif 100>scorefin>85:
+        print("Grade anda adalah A")
+    elif 85>scorefin>80:
+        print("Grade anda adalah A-")
+    elif 80>scorefin>75:
+        print("Grade anda adalah B")
+    elif 75>scorefin>70:
+        print("Grade anda adalah B-")
+    elif 0<scorefin<70:
+        print("Anda harus mengulang")
+    elif scorefin<0:
+        print("Nilai yang anda masukkan invalid")
+    if (input("Ulangi program? (Y/N) "))!="Y":
+        break
+    elif 0<scorefin<100:
+        break
